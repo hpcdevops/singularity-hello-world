b50da388fc5892631d45bb8cf8a1dec3035bb58f
HPCDevOPS <hpcdevops@users.noreply.github.com>
2017-07-18 22:48:55 -0700

    Import environment during bootstrap test

--- hello.sh
+++ hello.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [ -z ${HELLO_WORLD+x} ]; then
+    for f in $(find /.singularity.d/env -type f); do
+        source $f
+    done
+fi
+
 echo "San Diego Supercomputer Center - ${HELLO_WORLD}"
